80fc5bfebde231775728f3437b050bc285e59e5e
Shazia Nooruddin <shaziakn@Shazias-MacBook.local>
2020-06-13 22:16:00 -0500

    Finished challenge

--- Earthquake_Challenge/static/js/logic.js
+++ Earthquake_Challenge/static/js/logic.js
@@ -17,19 +17,29 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 	accessToken: API_KEY
 });
 
+// We create the dark view tile layer that will be an option for our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
 // Create a base layer that holds both maps.
 let baseMaps = {
 	"Streets": streets,
-	"Satellite Streets": satelliteStreets
+	"Satellite Streets": satelliteStreets,
+	"Light": light
   };
 
 // Create the earthquake layer for our map.
 let earthquakes = new L.layerGroup();
+let tectonic = new L.layerGroup();
 
 // We define an object that contains the overlays.
 // This overlay will be visible all the time.
 let overlays = {
-	Earthquakes: earthquakes
+	Earthquakes: earthquakes,
+	Tectonic_Plates: tectonic
 };
 
 // Create the map object with center, zoom level and default layer.
@@ -42,19 +52,10 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps, overlays).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/shaziakn/Mapping_Earthquakes/master/majorAirports.json";
-
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/shaziakn/Mapping_Earthquakes/master/torontoRoutes.json";
-
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/shaziakn/Mapping_Earthquakes/master/torontoNeighborhoods.json";
-
 // Create a style for the lines.
 let myStyle = {
-	fillColor: "#ffffa1",
-	weight: 1
+	color: "#ffffa2",
+	weight: 4
 }
 
 // Grabbing our GeoJSON data.
@@ -155,9 +156,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 	legend.addTo(map);
 	});
 
-	   
-
-		
-	
-
-
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {style: myStyle}).addTo(map).addTo(tectonic);
+	tectonic.addTo(map);
+});	
